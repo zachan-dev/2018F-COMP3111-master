4d607ab516a18a9c62fba2c5eb38479fbcc9659c
htchang1 <43569257+htchang1@users.noreply.github.com>
2018-11-30 20:48:39 +0800

    add javadoc

--- src/main/java/comp3111/webscraper/Item.java
+++ src/main/java/comp3111/webscraper/Item.java
@@ -8,22 +8,45 @@ public class Item implements Comparable<Item> {
 	private double price ;
 	private String url ;
 	private String date ;
-	
+	/**
+	 * Returns the title
+	 * @return String of title
+	 */
 	public String getTitle() {
 		return title;
 	}
+	/**
+	 * Set the title
+	 *@param title - the title you want to set
+	 */
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	/**
+	 * Returns the portal
+	 * @return String of portal
+	 */
 	public String getPortal() {
 		return portal;
 	}
+	/**
+	 * Set the portal
+	 *@param portal - the portal you want to set
+	 */
 	public void setPortal(String portal) {
 		this.portal = portal;
 	}
+	/**
+	 * Returns the price
+	 * @return double of price
+	 */
 	public double getPrice() {
 		return price;
 	}
+	/**
+	 * Set the price
+	 *@param price - the price you want to set
+	 */
 	public void setPrice(double price) {
 		this.price = price;
 	}
@@ -37,14 +60,21 @@ public class Item implements Comparable<Item> {
 	/**
 	 * Set the URL
 	 *@param url - the url you want to set
-	 * Returns the URL
 	 */
 	public void setUrl(String url) {
 		this.url = url;
 	}
+	/**
+	 * Returns the date
+	 * @return String of date
+	 */
 	public String getDate() {
 		return date;
 	}
+	/**
+	 * Set the date
+	 *@param date - the date you want to set
+	 */
 	public void setDate(String date) {	
 		this.date = date;
 	}
